1a8efa33749a0cce0ca2a3820c376b422dcbb876
Harry <harry@Harrys-MacBook-Air.local>
2023-04-08 17:08:57 +0530

    trying to put lottifiles

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import "animate.css/animate.min.css";
+
 
 const Intro = () => {
   return (
@@ -8,11 +10,14 @@ const Intro = () => {
         </h1>
 
 
-        <h1 className='text-white text-7xl ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-700'>
+        <h2 className='text-7xl ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-700'>
         asme manipal
-        </h1>
+        </h2>
 
-        
+        <p className='mt-5 text-white w-[450px] font-sans text-2xl'>We are a Premier Interdisciplinary Technical Club of MIT, Manipal. Experience of over half a decade and a pedigree of more than 140 years comes into fruition here.</p>
+
+
+
 
     </div>
   )
