fa32345d62d3c27c0fa06d2ea2b448097bb0413a
Harry <harry@Harrys-MacBook-Air.local>
2023-04-08 23:48:45 +0530

    before adding aos

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,15 +4,15 @@ import "@lottiefiles/lottie-player";
 const Intro = () => {
   return (
 
-    <div className="flex justify-center px-10">
-    <div className="flex flex-col w-full mx-auto p-4 max-w-[1240px] mt-20">
-      <h1 className="font-medium font-sans text-8xl text-white">we are</h1>
+    <div className="flex justify-between items-center px-10">
+    <div className="flex flex-col w-full mx-auto p-4 max-w-[1240px] ml-16 px-16">
+      <h1 className="flex flex-row font-medium font-sans text-8xl text-white">we are</h1>
 
       <h2 className="text-7xl ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-700">
         asme manipal
       </h2>
 
-      <p className="mt-5 text-white w-[450px] font-sans text-2xl">
+      <p className="mt-5 text-white w-[450px] font-sans text-2xl font-light">
         We are a Premier Interdisciplinary Technical Club of MIT, Manipal.
         Experience of over half a decade and a pedigree of more than 140 years
         comes into fruition here.
@@ -21,7 +21,7 @@ const Intro = () => {
       
     </div>
 
-    <div className="flex flex-col">
+    <div className="flex flex-col mr-16 mt-8 mx-auto">
         <lottie-player
           autoplay
           loop
