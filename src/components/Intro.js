c23192e29e86a2674bd9b132e61e7331e0cf8b70
Harry <harry@Harrys-MacBook-Air.local>
2023-04-08 13:55:16 +0530

    trying to put aos

---
+++ src/components/Intro.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className='flex flex-col w-full mx-auto p-4 max-w-[1240px] mt-20'>
+        <h1 className='font-medium font-sans text-8xl text-white'>
+        we are 
+        </h1>
+
+
+        <h1 className='text-white text-7xl ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-700'>
+        asme manipal
+        </h1>
+
+        
+
+    </div>
+  )
+}
+
+export default Intro
