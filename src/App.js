69e92600dc3e296c16cbcde45b53823f9f942a82
Harry <harry@Harrys-MacBook-Air.local>
2023-04-09 00:30:21 +0530

    trying video player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
+import About from './components/About';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
     <Navbar />
     <Intro/>
+    <About/>
 
 
     </div>
