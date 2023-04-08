e46c65b85ecbaa6f6dd889d882460c95d676fb8c
Harry <harry@Harrys-MacBook-Air.local>
2023-04-09 00:09:43 +0530

    added intro aos

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "@lottiefiles/lottie-player";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Intro = () => {
+
+    AOS.init();
+  
+
   return (
 
     <div className="flex justify-between items-center px-10">
-    <div className="flex flex-col w-full mx-auto p-4 max-w-[1240px] ml-16 px-16">
+    <div className="flex flex-col w-full mx-auto p-4 max-w-[1240px] ml-16 px-16" data-aos="zoom-in-right" data-aos-duration="1500" data-aos-delay='200' >
       <h1 className="flex flex-row font-medium font-sans text-8xl text-white">we are</h1>
 
       <h2 className="text-7xl ont-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-blue-700">
@@ -21,7 +27,7 @@ const Intro = () => {
       
     </div>
 
-    <div className="flex flex-col mr-16 mt-8 mx-auto">
+    <div className="flex flex-col mr-16 mt-8 mx-auto" data-aos="zoom-in-left" data-aos-duration="1500" data-aos-delay='200'>
         <lottie-player
           autoplay
           loop
@@ -31,6 +37,7 @@ const Intro = () => {
         ></lottie-player>
       </div>
 
+      
     </div>
   );
 };
